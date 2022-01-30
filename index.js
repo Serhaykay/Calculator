b322caad5998bc5ff20868f62b92b55767a8cd17
Serhaykay <92628969+Serhaykay@users.noreply.github.com>
2022-01-30 16:46:24 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function inputNumbers(x){
+     let num = document.getElementById("display").value += x
+}
+function clearNumbers(y){
+     let clear = document.getElementById("display").value = y
+}
+function answer(){
+     let result = eval(document.getElementById("display").value);
+     document.getElementById("display").value=result;
+}
+
+
+
+
+
+
+
