8e3ab9339e60220654eee3872b83888f89367e59
Ser Haykay <92628969+Serhaykay@users.noreply.github.com>
2024-02-22 23:40:01 +0100

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,14 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+  
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
+  function calculateResult() {
+    const expression = document.getElementById('display').value;
+    const result = eval(expression);
+    document.getElementById('display').value = result;
+  }
+  
